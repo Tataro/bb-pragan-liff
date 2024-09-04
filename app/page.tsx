39c7520e1c162097bf381ab2600e8f15b64426b6
Tataro <tat.kittitat@gmail.com>
2024-09-04 16:31:42 +0700

    chore: Update dependencies and implement Line Login
    ```

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,26 @@ import Card from "./ui/Card";
 
 export default function Home() {
   const mockData = [
-    { title: "Card 1", imageUrl: "/image1.jpg" },
-    { title: "Card 2", imageUrl: "/image2.jpg" },
-    { title: "Card 3", imageUrl: "/image3.jpg" },
-    { title: "Card 4", imageUrl: "/image4.jpg" },
-    { title: "Card 5", imageUrl: "/image5.jpg" },
+    {
+      title: "Card 1",
+      imageUrl: "https://cataas.com/cat?width=800&height=600",
+    },
+    {
+      title: "Card 2",
+      imageUrl: "https://cataas.com/cat?width=700&height=600",
+    },
+    {
+      title: "Card 3",
+      imageUrl: "https://cataas.com/cat?width=600&height=600",
+    },
+    {
+      title: "Card 4",
+      imageUrl: "https://cataas.com/cat?width=500&height=600",
+    },
+    {
+      title: "Card 5",
+      imageUrl: "https://cataas.com/cat?width=400&height=600",
+    },
   ];
 
   return (
--- app/ui/TopNav.tsx
+++ app/ui/TopNav.tsx
@@ -1,19 +1,25 @@
 // components/TopNav.tsx
 import Image from "next/image";
 
-export default function TopNav() {
+export default function TopNav({
+  user,
+}: {
+  user: { name: string; pictureUrl: string } | undefined;
+}) {
+  //   console.log("user", user);
+
   return (
     <nav className="flex justify-between items-center bg-white p-4 shadow-md">
       <div className="text-xl font-bold">BB Pragan</div>
       <div className="flex items-center space-x-4">
         <Image
-          src="/profile.jpg"
+          src={user?.pictureUrl ?? "/default-profile.png"}
           alt="Profile Picture"
           width={40}
           height={40}
           className="rounded-full"
         />
-        <span className="font-medium">User Name</span>
+        <span className="font-medium">{user?.name ?? "-"}</span>
       </div>
     </nav>
   );
